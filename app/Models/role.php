80867d9c9c08f58e5c86e0858be6099243e300eb
Rendy <chrismaskampong@gmail.com>
2026-05-03 22:03:06 +0800

    Add model classes for Category, Item, Order, OrderItem, and Role with relationships

---
+++ app/Models/role.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class role extends Model
+{
+    use SoftDeletes;
+    protected $fillable = ['name', 'description', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
